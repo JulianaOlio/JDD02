f04fbce6924ff2e65195636287a4141c1505bf81
Juliana <jullymary@hotmail.com>
2025-09-15 22:15:32 -0300

    atualização do pacote

---
+++ intro-js/intro.js
@@ -0,0 +1,21 @@
+console.log('Bela js ta com fome ainda!');
+
+//variaveis em js não tem tipo
+// no JS a tipagem é feita de forma dinamica, no momento da atrbuição do valor
+// devemos declarar variveis usando let ou const
+let nomeUsuario ='Bela';
+let idUsuario = 123456;
+let emailUsuaurio = 'bela@fiap.com.br'
+let logado = true;
+
+console.log(nomeUsuario, idUsuario);
+console.log(emailUsuaurio, logado);
+console.log(typeof nomeUsuario, nomeUsuario);
+console.log(typeof idUsuario, idUsuario);
+console.log(typeof emailUsuaurio, emailUsuaurio);
+console.log(typeof logado, logado);
+
+//Arrays em JS
+const pessoas = ['Eu', 'Tu', 'Ele', 'Nós', 'Vós', 'Eles'];
+console.log(pessoas);
+console.table(pessoas)
